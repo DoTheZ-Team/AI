499f0151b8637b8738d7acd0658e5970623df28c
이주애 <joojuae02@gachon.ac.kr>
2024-04-29 20:55:15 +0900

    fix: hashtag 주기적으로 업데이트하는 함수 추가

--- api/recommend.py
+++ api/recommend.py
@@ -5,16 +5,9 @@ from models.recommendation import Recommendation, Recommendations
 
 router = APIRouter()
 
+# 추천 결과 반환 API
 @router.get("/user/{member_id}/recommend", response_model=Recommendations)
 async def recommend_user(member_id: int):
-    # if(새로운 해시태그 추가): 아래의 함수 실행: 새로운 해시태그 추가 부분을 어떻게 진행해야 될지 몰라서 일단 보류
-    # svs.update_hashtag(member_id, new_hashtag, res)
-    # tf-idf 수행
-    tfidf_matrix, data = svs.TF_IDF()
-    es_client = svs.index_creation(tfidf_matrix)
-    docs = svs.prepare_documents(tfidf_matrix)
-    svs.indexing(docs, es_client)
-   
     # if(해시태그가 기존 그대로임): TF-IDF 수행 안하고 바로 아래 함수부터 실행
     # res = svs.find_hashtags(member_id)
     response = await svs.recommend_user(member_id, es_client)
