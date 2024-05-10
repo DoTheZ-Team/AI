5ad77db2782014e3a838678947d93cbad35a01ca
이주애 <joojuae02@gachon.ac.kr>
2024-05-10 15:09:49 +0900

    add: dockerfile 작성

--- core/config.py
+++ core/config.py
@@ -7,7 +7,6 @@ import requests
 def get_config():
     config_url = "http://localhost:8888/recommend-dev/default"
     response = requests.get(config_url)
-    print(response.json())
     return response.json()
 
 config = get_config()
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
 from api.recommend import router as recommend_router
 from db.database import es_client, close_elasticsearch
-from py_eureka_client import eureka_client
-from core.config import config
+import uvicorn
 
-app = FastAPI()
+app = FastAPI(
+    title="Recommend-Service")
 
 @app.on_event("startup")
 async def startup_event():
@@ -16,3 +16,6 @@ async def shutdown_event():
     await close_elasticsearch()
     
 app.include_router(recommend_router)
+
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="127.0.0.1", port=8080)
