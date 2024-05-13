469a1af1ec913b2d7c75948be33fb1c61346fd85
이주애 <joojuae02@gachon.ac.kr>
2024-05-13 11:43:00 +0900

    add: requirements.txt 추가

--- core/config.py
+++ core/config.py
@@ -1,7 +1,7 @@
 import requests
 
 def get_config():
-    config_url = "http://localhost:8888/recommend-dev/default"
+    config_url = "http://glue-config.glue-service.svc.cluster.local:8888/recommend-dev/default"
     response = requests.get(config_url)
     print(response.json())
     return response.json()
