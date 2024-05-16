3eec27eedbfd29ca2bfc49cca8e96a8f46ce3ca1
이주애 <joojuae02@gachon.ac.kr>
2024-05-16 16:51:24 +0900

    fix: 에러 메시지 출력 추가

--- core/config.py
+++ core/config.py
@@ -7,12 +7,13 @@ dotenv.load_dotenv('.env')
 
 def get_config():
     try: 
-        #response = requests.get(os.environ.get("CONFIG_URL"))
-        response = requests.get("http://glue-config.glue-service.svc.cluster.local:8888/recommend-dev/default")
-
+        config_url = os.environ.get("CONFIG_URL")
+        if not config_url:
+            raise ValueError("CONFIG_URL 환경 변수가 설정되지 않았습니다.")
+        print(f"CONFIG_URL: {config_url}")  # 디버깅 출력
+        response = requests.get(config_url)
         print(response.json())
         return response.json()
-    
     except requests.exceptions.HTTPError as e:
         ErrorHandler.handle_requests_http_error(e, "서버에서 설정을 가져오는데 실패했습니다.")
     except requests.exceptions.RequestException as e:
